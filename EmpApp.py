3a000eb44bb051f97bf3f370607c6c49e3f13a90
weyzhen <86113280+weyzhen@users.noreply.github.com>
2022-04-07 08:40:11 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -86,7 +86,7 @@ def AddEmp():
 @app.route("/getemp", methods=['GET'])
 def GetEmp():
     emp_id = request.args.get('emp_id')
-    return render_template('GetEmp.html', name=emp_name)
+    return render_template('GetEmp.html', getemp=getemp)
 
 
 if __name__ == '__main__':
