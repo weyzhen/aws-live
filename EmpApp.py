dedbaa4d20570fa84658d255d2172831364399ad
weyzhen <86113280+weyzhen@users.noreply.github.com>
2022-04-07 23:37:41 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -81,7 +81,7 @@ def AddEmp():
         cursor.close()
 
     print("all modification done...")
-    return render_template('AddEmp.html', name=emp_name)
+    return render_template('AddEmpOutput.html', name=emp_name)
 
 @app.route("/getemp", methods=['GET'])
 def GetEmp():
